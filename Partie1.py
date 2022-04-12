b671f6c97a8f7517cf5773f59ba863976238555b
user <francois.gallois.pro@gmail.com>
2022-04-12 15:00:06 +0200

    Création d'une liste et d'une itération dessus pour la création d'arrêtes

--- Partie1.py
+++ Partie1.py
@@ -1,14 +1,14 @@
 class graph() :
     def __init__(self) :
         self.dico = {}
-        
+
     def __str__(self):
         return str(self.dico)
-    
+
     def addSommet(self,sommet):
         if sommet not in self.dico:
             self.dico[sommet] = []
-        
+
     def addArete(self,sa,sb) :
         self.dico[sa].append(sb)
         self.dico[sb].append(sa)
@@ -28,39 +28,40 @@ for i in range(verticale):
     counta+=1
     countb = 1
 
-LstSommet.addArete((1,1),(2,1))
-LstSommet.addArete((2,1),(2,2))
-LstSommet.addArete((2,2),(3,2))
-LstSommet.addArete((3,2),(4,2))
-LstSommet.addArete((4,2),(4,3))
-LstSommet.addArete((4,3),(4,4))
+arretes = [
+        [(1,1),(2,1)],
+        [(2,1),(2,2)],
+        [(2,2),(3,2)],
+        [(3,2),(4,2)],
+        [(4,2),(4,3)],
+        [(4,3),(4,4)],
+        [(2,2),(2,3)],
+        [(2,3),(1,3)],
+        [(1,3),(1,4)],
+        [(1,4),(2,4)],
+        [(1,4),(1,5)],
+        [(2,4),(3,4)],
+        [(3,4),(3,5)],
+        [(3,5),(3,6)],
+        [(3,6),(4,6)],
+        [(4,6),(4,5)],
+        [(3,6),(3,7)],
+        [(3,7),(4,7)],
+        [(4,7),(4,8)],
+        [(3,8),(4,8)],
+        [(2,8),(3,8)],
+        [(2,8),(1,8)],
+        [(1,7),(1,8)],
+        [(1,7),(1,6)],
+        [(2,6),(1,6)],
+        [(2,6),(3,6)],
+        [(2,6),(2,7)],
+        [(3,7),(2,7)],
+        [(1,4),(1,5)],
+        [(2,5),(1,5)],
+        [(2,5),(2,6)]]
 
-LstSommet.addArete((2,2),(2,3))
-LstSommet.addArete((2,3),(1,3))
-LstSommet.addArete((1,3),(1,4))
-LstSommet.addArete((1,4),(2,4))
-LstSommet.addArete((1,4),(1,5))
-LstSommet.addArete((2,4),(3,4))
-LstSommet.addArete((3,4),(3,5))
-LstSommet.addArete((3,5),(3,6))
-LstSommet.addArete((3,6),(4,6))
-LstSommet.addArete((4,6),(4,5))
-
-LstSommet.addArete((3,6),(3,7))
-LstSommet.addArete((3,7),(4,7))
-LstSommet.addArete((4,7),(4,8))
-LstSommet.addArete((3,8),(4,8))
-LstSommet.addArete((2,8),(3,8))
-LstSommet.addArete((2,8),(1,8))
-LstSommet.addArete((1,7),(1,8))
-LstSommet.addArete((1,7),(1,6))
-LstSommet.addArete((2,6),(1,6))
-LstSommet.addArete((2,6),(3,6))
-LstSommet.addArete((2,6),(2,7))
-LstSommet.addArete((3,7),(2,7))
-
-LstSommet.addArete((1,4),(1,5))
-LstSommet.addArete((2,5),(1,5))
-LstSommet.addArete((2,5),(2,6))
+for i in arretes:
+    LstSommet.addArete(i[0],i[1])
 
 print(LstSommet)
